b63a61e73898693e48cfc6877b2fb904b0246f70
kanghw <rkdgeumchi22@gmail.com>
2026-06-04 20:38:39 +0900

    js-learning

---
+++ web05_aboretum/js/web05_arboretum.js
@@ -0,0 +1,18 @@
+let login = document.querySelector("#h_box2");
+    login.onclick = function() {
+      login.style.color = "red";
+    } 
+let join = document.querySelector("#h_box3");
+    join.onclick = function() {
+      join.style.color = "blue";
+    }
+
+
+// alert("이상적인 수목원에 오신걸 환영합니다.");
+
+// confirm("로그아웃하시겠습니까?");
+
+// let name = prompt("당신의 이름은 무엇입니까?");
+
+document.write("<b>"+name+"</b>이군요<br>");
+console.log("<b>"+name+"</b>이군요, 반갑습니다.<br>")
